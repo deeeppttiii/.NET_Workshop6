956110df23ee6ecacccab231b0d5bde5c11b23a0
Deepti Dhungana <deeptidhungana322@gmail.com>
2025-12-03 11:18:45 +0545

    Task 1 Code Completed

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Create rectangle object
+            Rectangle rect = new Rectangle();
+
+            // Assign values
+            rect.Length = 10.5;
+            rect.Breadth = 5.2;
+
+            // Display details
+            Console.WriteLine(rect.ShowDetails());
+
+            // Calculate and display area
+            Console.WriteLine($"Area: {rect.GetArea()}");
+
+            // Calculate and display perimeter
+            Console.WriteLine($"Perimeter: {rect.GetPerimeter()}");
+
+            Console.ReadLine(); // Wait for user to close console
         }
     }
 }
---
+++ Task1/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Rectangle
+    {
+        private double length;
+        private double breadth;
+
+        // Expression-bodied properties
+        public double Length
+        {
+            get => length;
+            set => length = value;
+        }
+
+        public double Breadth
+        {
+            get => breadth;
+            set => breadth = value;
+        }
+
+        // Expression-bodied method to calculate area
+        public double GetArea() => length * breadth;
+
+        // NEW: Expression-bodied method to calculate perimeter
+        public double GetPerimeter() => 2 * (length + breadth);
+
+        // Expression-bodied method to show details
+        public string ShowDetails() => $"Length: {length}, Breadth: {breadth}";
+    }
+}
