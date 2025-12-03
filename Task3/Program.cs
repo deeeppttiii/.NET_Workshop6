50a0d592431a887ae66db838f7fe8dad8fdee26d
Deepti Dhungana <deeptidhungana322@gmail.com>
2025-12-03 11:59:00 +0545

    Task 3 Code Completed

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Task3
+﻿using System;
+
+namespace FuncExample
 {
-    internal class Program
+    class Program
     {
+        // Method that processes numbers based on a condition
+        static void ProcessNumbers(int[] numbers, Func<int, bool> condition)
+        {
+            foreach (int num in numbers)
+            {
+                if (condition(num))
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = { 5, 12, 7, 20, 3, 14, 8 };
+
+            Console.WriteLine("Even numbers:");
+            // Using Func delegate to filter even numbers
+            ProcessNumbers(numbers, num => num % 2 == 0);
+
+            Console.WriteLine("\nNumbers greater than 10:");
+            // Using Func delegate to filter numbers greater than 10
+            ProcessNumbers(numbers, num => num > 10);
         }
     }
 }
